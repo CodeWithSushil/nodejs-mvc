971286c54e57b56a81bf7a663c00ac8ac5bc88ea
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-01-15 14:51:50 +0530

    update css

--- app.mjs
+++ app.mjs
@@ -1,19 +1,18 @@
-import express from 'express';
-import Router from './routes/router.js';
-const PORT = process.env.PORT | 3000;
-import ejs from 'ejs';
+import express from "express";
+import { Routes } from "./routes/router.js";
 
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.urlencoded({
-  extended: true
-}));
+const PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(Router);
+app.set("view engine", "ejs");
+// app.set("views", "./views")
+
+// express router
+app.use(Routes);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
