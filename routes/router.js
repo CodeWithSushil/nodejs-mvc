d7cc480c4a8e85cc0b3fe46ca23280f77c6a3c0c
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-01-15 12:48:58 +0530

    update router.js

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,14 @@
-export default Router;
+import { Router } from 'express';
+import path from 'path';
+import { readFile, writeFile } from 'fs/promises';
 
-app.get('/', (req, res) => {
-  res.send('Hello, Sushil!');
+const router = Router();
+
+const DATA_FILE = path.join('data', 'links.json');
+
+router.get('/', (req, res) => {
+  //res.render();
+  res.send('<h1>Hello, Sushil!</h1>');
 });
+
+export default router;
